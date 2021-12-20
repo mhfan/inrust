c84c9c913581f1beb747979a4b8046c563aa18c3
MeiHui FAN <mhfan@ustc.edu>
2021-12-20 19:06:39 +0800

    decorated refinement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 
 use std::{io::{self, Write}, cmp::Ordering};
-
-use rand::Rng;
 //pub use A::B:C as D;
+use rand::Rng;
 
 //#[allow(unused_macros)]
-//macro_rules! var_args { ($($args: expr), *) => {{ }} }  //$(f($args);)*     // XXX
+//macro_rules! var_args { ($($args: expr), *) => {{ }} }  //$(f($args);)*   // XXX
 //macro_rules! printvar { ($var: expr) => { println!("{}: {:?}", stringify!($var), $var); } }
 
 // src/main.rs (default application entry point)
@@ -27,27 +26,33 @@ fn main()/* -> Result<(), Box<dyn Error>>*/ {
 fn  guess_number() {
     let (max, lang) = (100, true);
 
-    if  lang { println!("### 猜数字游戏 (1-{}) ###", max) } else {
-        println!("Guess the number (1-{})", max);
+    if  lang {  println!("### 猜数字游戏 (1-{}) ###", max) } else {
+                println!("Guess the number (1-{})", max);
     }
 
-    let secret = rand::thread_rng().gen_range(1..=max);     //dbg!(secret);
+    let secret = rand::thread_rng().gen_range(1..=max); //dbg!(secret);
 
-    let _result = 'label: loop {
-        if lang { print!("\n输入你猜的数字: ") } else { print!("\nInput a number you guess: ") }
-        io::stdout().flush().expect("Failed to flush"); //.unwrap();
+    let _result = 'label: loop {    // unused prefixed with underscore
+        if lang {   print!("\n输入你猜的数字: ") } else {
+                    print!("\nInput a number you guess: ")
+        }
 
         let mut guess = String::new();
+        io::stdout().flush().expect("Failed to flush"); //.unwrap();
         io::stdin().read_line(&mut guess).expect("Failed to read!");
 
-        //let guess: i32 = guess.parse().expect("Please type a number");
+        //let guess: i32 = guess.trim().parse().expect("Please type a number");
 
         //match guess.trim().parse::<i32>() { Ok(_guess) => { }, _ => () }
         if let Ok(guess) = guess.trim().parse::<i32>() { // isize
             match guess.cmp(&secret) {
-                Ordering::Greater   =>   if lang { println!("[太大了]") } else { println!("[Too large]") },
-                Ordering::Less      =>   if lang { println!("[太小了]") } else { println!("[Too small]") },
-                Ordering::Equal     => { if lang { println!("[猜对了]") } else { println!("[Bingo!]") } break 1 }
+                Ordering::Greater =>
+                    if lang { println!("[太大了]") } else { println!("[Too large]") },
+                Ordering::Less    =>
+                    if lang { println!("[太小了]") } else { println!("[Too small]") },
+                Ordering::Equal   => {
+                    if lang { println!("[猜对了]") } else { println!("[Bingo!]") } break 1
+                }
             }
         } else { guess.make_ascii_lowercase();  //guess.to_lowercase();
             if   guess.trim() == "quit" { break 'label 0 }
