3f1d94c397598bcb80b223c32e76b473349be3cb
MeiHui FAN <mhfan@ustc.edu>
2022-01-20 11:08:18 +0800

    rename to hello-rust

--- tests/test.rs
+++ tests/test.rs
@@ -1,11 +1,11 @@
 
-//use study_rust;
+//use hello_rust;
 
 //mod common;   // subdirectory module
 
 #[test]
 fn sample_test() {
-    assert_eq!(study_rust::f(), 0);
+    assert_eq!(hello_rust::f(), 0);
     // Integration tests (and benchmarks) 'depend' to the crate
     // like a 3rd party would. Hence, they only see public items.
 }
