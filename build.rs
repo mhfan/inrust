64b8e36b48ccafd3fb04d249bcb01c81780852a3
MeiHui FAN <mhfan@ustc.edu>
2022-07-25 11:57:22 +0800

    roll back to -std=c++20 by running on ubuntu-22.04

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("cargo:rerun-if-changed={comp24_cpp}");
     println!("cargo:rerun-if-changed=src/comp24.h");
 
-    #[cfg(feature = "cc")] cc::Build::new().cpp(true).flag("-std=c++2a").opt_level(2)
+    #[cfg(feature = "cc")] cc::Build::new().cpp(true).flag("-std=c++20").opt_level(2)
         .define("NDEBUG", None) //.define("USE_LIST", None)//.define("RUN_TEST", None)
         .file(comp24_cpp).compile("comp24");   // libcomp24.a
 }
