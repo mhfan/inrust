43a827cd0f455827a61c024ba15f9b58aa05ec04
MeiHui FAN <mhfan@ustc.edu>
2022-08-22 15:34:09 +0800

    combine calc24.h into calc24.cpp for simplification,
    as cxx bridge doesn't work yet.

--- src/calc24.cpp
+++ src/calc24.cpp
@@ -16,7 +16,53 @@
 #include <iostream>
 #include <algorithm>
 
+#ifndef CALC24_H
+#define CALC24_H
+
+#include <cstdint>
+#include <memory>   // shared_ptr
+
+template <typename T> struct RNum { T n, d; RNum(auto n, T d = 1): n(n), d(d) {} };
+typedef RNum<int32_t> Rational;
+
+//typedef char Oper;
+enum Oper: char { Num, Add = '+', Sub = '-', Mul = '*', Div = '/', };
+
+struct Expr;
+typedef std::shared_ptr<const Expr> PtrE;
+
+struct Expr {
+    Rational v; PtrE a, b; Oper op;     // anonymous structure
+
+    Expr(auto n): Expr(Rational(n)) {}  // Constructor delegation
+    Expr(const Rational& r, Oper op = Num,  // a & b refer to left & right operands
+         const PtrE& a = nullptr, const PtrE& b = nullptr): v(r), a(a), b(b), op(op) {}
+
+    //Expr(): Expr(Rational(0, 0)) {}
+    //Expr(const Expr&) = delete;
+    //~Expr();
+};
+
+#include <vector>
+using std::vector;
+
+typedef   enum Calc24Algo: uint8_t { DynProg, SplitSet, Inplace, Construct } Calc24Algo;
+typedef struct Calc24IO {
+    const Calc24Algo algo; bool ia;
+    const Rational goal, *const nums;
+    const size_t ncnt;
+
+    size_t ecnt;
+    const char* *exps;
+    //const PtrE const *exps;
+    //const Expr* const *exps;
+}   Calc24IO;
+
+extern "C" void calc24_algo(Calc24IO* calc24);
+
+#else
 #include "calc24.h"
+#endif//CALC24_H
 
 /* inline auto operator+(const Rational& lhs, const auto& rhs) noexcept {
     return Rational(lhs.n * rhs.d + lhs.d * rhs.n, lhs.d * rhs.d); }
