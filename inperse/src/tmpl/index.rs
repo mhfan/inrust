cb762603186e6b3f83996a5ee7dda7c2a52670f2
MeiHui FAN <mhfan@ustc.edu>
2022-11-19 21:25:41 +0800

    move github-corner to seperate html

--- inperse/src/tmpl/index.rs
+++ inperse/src/tmpl/index.rs
@@ -58,45 +58,8 @@ impl Game24 {
 
 #[perseus::template_rx] pub fn index_page<'a, G: Html>(cx: Scope<'a>,
     state: IndexPageStateRx<'a>) -> View<G> {
+    //let gh_corner = view! { cx, };
     //#[component] fn gh_corner<G: Html>(cx: Scope) -> View<G> { }
-    let gh_corner = view! { cx,
-        a(href={ env!("CARGO_PKG_REPOSITORY") },
-            class="github-corner", aria-label="View source on GitHub") {
-          svg(width="60", height="60", viewBox="0 0 250 250", aria-hidden="true",
-                style="fill: #ddd; color: #151513; position: absolute;
-                top: 0; border: 0; right: 0;") {
-
-            path(d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z")
-            path(d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6
-                120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3
-                C122.9,97.6 130.6,101.9 134.4,103.2", style="transform-origin: 130px 106px;",
-                fill="currentColor", class="octo-arm")
-
-            path(d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2
-                139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2
-                159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2
-                C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2
-                216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8
-                198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9
-                152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z",
-                fill="currentColor", class="octo-body")
-          }     // https://github.com/tholman/github-corners
-
-            style { ".github-corner:hover .octo-arm {
-                        animation: octocat-wave 560ms ease-in-out }
-                @keyframes octocat-wave {
-                    0%,100% { transform: rotate(0) }
-                    20%,60% { transform: rotate(-25deg) }
-                    40%,80% { transform: rotate(10deg) }
-                }
-                @media (max-width: 500px) {
-                     .github-corner:hover .octo-arm { animation: none }
-                     .github-corner       .octo-arm {
-                        animation: octocat-wave 560ms ease-in-out }
-                }"
-            }
-        }
-    };
 
     let num_class  = "px-4 py-2 my-4 w-fit appearance-none select-text
         read-only:bg-transparent bg-stone-200 border border-purple-200
@@ -144,13 +107,15 @@ impl Game24 {
         } else if inp.focus().is_ok() { inp.select() }
     };
 
-    view! { cx,     //header { } main { } footer { }
+    view! { cx,
         // <!--#include file="gh-corner.html" -->
         // https://en.wikipedia.org/wiki/Server_Side_Includes
         //object(type="text/html", data=".perseus/static/gh-corner.html")
-        //div(dangerously_set_inner_html="...")
 
-        h1(class="text-4xl m-4") {  (gh_corner)     // interpolation
+        header(class="text-4xl m-4") {  //(gh_corner)     // interpolation
+            a(href=env!("CARGO_PKG_REPOSITORY"),
+                dangerously_set_inner_html=include_str!("../../static/gh-corner.html"),
+                class="github-corner", aria-label="View source on GitHub")
             a(href="https://github.com/mhfan/inrust") { (t!("header", cx)) }
         }
 
@@ -270,7 +235,7 @@ impl Game24 {
             }}} else { view! { cx, } })
         }
 
-        p(class="m-4") { span { (t!("copyright", cx)) } " by "
+        footer(class="m-4") { span { (t!("copyright", cx)) } " by "
             a(href="https://github.com/mhfan") { "mhfan" } } // XXX: move to index_view/footer?
     }
 }
