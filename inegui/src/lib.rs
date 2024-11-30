2495fd0396c8fda3ad13e4c4c81bd9f33919e7ee
MeiHui FAN <mhfan@ustc.edu>
2024-11-30 10:29:41 +0800

    remove incomplete inslint & inegui code

--- inegui/src/lib.rs
+++
@@ -1,8 +0,0 @@
-
-#![warn(clippy::all, rust_2018_idioms)]
-
-mod app;
-mod backend_panel;
-pub use app::TemplateApp;
-pub(crate) mod frame_history;
-
--- inslint/build.rs
+++
@@ -1,5 +0,0 @@
-
-fn main() {
-    slint_build::compile("ui/game24-app.slint").unwrap();
-}
-
